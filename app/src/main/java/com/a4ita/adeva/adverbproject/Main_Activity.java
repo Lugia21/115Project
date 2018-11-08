4d32c6ef30d30d1af99b1ab61b40fe58550d654c
Lugia21 <kevinadeva@ymail.com>
2018-11-08 10:27:06 +0800

    Finished Splash Screen, sidemenu WIP

---
+++ app/src/main/java/com/a4ita/adeva/adverbproject/Main_Activity.java
@@ -0,0 +1,35 @@
+package com.a4ita.adeva.adverbproject;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toolbar;
+
+
+public class Main_Activity extends AppCompatActivity {
+
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        mDrawerList = (ListView) findViewById(R.id.navList);
+
+        addDrawerItems();
+
+    }
+
+    private void addDrawerItems() {
+        String[] osArray = { "Android", "iOS", "Windows", "OS X", "Linux" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
+    }
+}
--- app/src/main/java/com/a4ita/adeva/adverbproject/SplashScreen.java
+++ app/src/main/java/com/a4ita/adeva/adverbproject/SplashScreen.java
@@ -1,13 +1,28 @@
 package com.a4ita.adeva.adverbproject;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
 
+    Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, Main_Activity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+
     }
 }
